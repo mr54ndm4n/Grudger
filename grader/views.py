ea76bb9f9ff0e46a6086c2857152aebae540d43a
isara <isaradream@gmail.com>
2016-10-20 03:25:14 +0700

    Created Problem_list Page

--- grader/views.py
+++ grader/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Student, Problem, Submission, ProblemCategorie, Lesson
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+import time
+import datetime
+from django.utils import timezone
 # Create your views here.
 
 def basic_info(request):
@@ -49,10 +52,20 @@ def syllabus(request):
 
 @login_required(login_url='/login/')
 def problem_list(request):
-	data = {'page': 'problem'}
-	data.update(basic_info(request))
-	print(data)
-	return render(request, "problem_list.html", data)
+	user = request.user
+	student = Student.objects.get(user = request.user)
+	problem_list = Problem.objects.all()
+	sub_prob = []
+	for problem in problem_list:
+		try:
+			ps = Submission.objects.get(student = student, problem = problem)
+		except:
+			ps = Submission(student = student, problem = problem)
+			ps.save()
+		print('Active: ' + str(problem.catagories.due >= timezone.now() and problem.catagories.start <= timezone.now()))
+		active = problem.catagories.due >= timezone.now() and problem.catagories.start <= timezone.now()
+		sub_prob.append({'problem': problem, 'submission': ps, 'active': active})
+	return render(request, 'problem_list.html', {'user': user, 'student': student, 'sub_prob_list': sub_prob})
 
 
 @login_required(login_url='/login/')
